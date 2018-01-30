45981b3675bdbf4961fda11651be28cf146ea3f7
Geddings Barrineau <cbarrin@g.clemson.edu>
2018-01-30 16:03:43 -0500

    Adding script to tar everything.

---
+++ tar-everything.sh
@@ -0,0 +1,5 @@
+tar cvfz client-install.tar.gz client-install.sh
+tar cvfz controller-install.tar.gz controller-install.sh
+tar cvfz ovs-install.tar.gz ovs-install.sh
+tar cvfz router-install.tar.gz router-install.sh
+tar cvfz server-install.tar.gz server-install.sh
